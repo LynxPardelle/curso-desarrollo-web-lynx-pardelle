4c119ae04a8f1a390c34ea34a17df8612922638b
Lynx Pardelle <lnxdrk@gmail.com>
2024-04-29 02:28:50 -0600

    feat: plan de desarrollo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,172 @@ export class AppComponent {
     '[ALT] + click = Crear cursores nuevos por cada click en diferentes lados.',
     'CTRL + F = Abrir el buscador',
   ];
+
+  public plan = {
+    metas: [
+      {
+        title: 'Blog Genérico',
+        caracteristicas: [
+          {
+            caracteristica: 'Poder manipular artículos',
+          },
+          {
+            caracteristica: 'Poder configurar el sitio',
+          },
+        ],
+        objetivos: [
+          {
+            objetivo: 'Tener la planeación del proyecto',
+            tareas: [
+              {
+                tarea:
+                  'Definir el objetivo del proyecto(Propuesta del proyecto)',
+                hecho: true,
+                etapa: 'acabado',
+              },
+              {
+                tarea: 'Definir las características del proyecto',
+                hecho: true,
+                etapa: 'acabado',
+              },
+              {
+                tarea: 'Definir los procesos del desarrollo del proyecto',
+                hecho: true,
+                etapa: 'acabado',
+              },
+              {
+                tarea:
+                  'Definir los pasos de los procesos del desarrollo del proyecto',
+                hecho: true,
+                etapa: 'acabado',
+              },
+            ],
+          },
+          {
+            objetivo: 'Tener el diseño del proyecto',
+            tareas: [
+              {
+                tarea: 'Análisis de los requerimientos',
+                hecho: true,
+                etapa: 'acabado',
+              },
+              {
+                tarea: 'Análisis de competidores',
+                hecho: false,
+                etapa: 'cancelado',
+              },
+              {
+                tarea: 'Diseño de la base de datos',
+                hecho: true,
+                etapa: 'acabado',
+              },
+              {
+                tarea: 'Diseño de la interfaz',
+                hecho: false,
+                etapa: 'cancelado',
+              },
+              {
+                tarea: 'Inventario de las páginas',
+                hecho: true,
+                etapa: 'acabado',
+              },
+              {
+                tarea: 'Matriz de pruebas',
+                hecho: false,
+                etapa: 'no empezado',
+              },
+            ],
+          },
+          {
+            objetivo: 'Tener el código del proyecto',
+            tareas: [
+              {
+                tarea: 'Crear el repositorio',
+                hecho: false,
+                etapa: 'no empezado',
+              },
+              {
+                tarea:
+                  'Crear la estructura del proyecto(carpetas y archivos) Frontend',
+                hecho: false,
+                etapa: 'no empezado',
+              },
+              {
+                tarea: 'Implementar el diseño de la aplicación',
+                hecho: false,
+                etapa: 'no empezado',
+              },
+              {
+                tarea:
+                  'Crear la estructura del proyecto(carpetas y archivos) Backend',
+                hecho: false,
+                etapa: 'no empezado',
+              },
+              {
+                tarea:
+                  'Crear los CRUDs(Create Read Update Delete) de los modelos de las bases de datos',
+                hecho: false,
+                etapa: 'no empezado',
+              },
+              {
+                tarea: 'Integración de la API',
+                hecho: false,
+                etapa: 'no empezado',
+              },
+            ],
+          },
+          {
+            objetivo: 'Tener la configuración del servidor del proyecto',
+            tareas: [
+              {
+                tarea: 'Configurar el servidor',
+                hecho: false,
+                etapa: 'no empezado',
+              },
+              {
+                tarea: 'Configurar la base de datos',
+                hecho: false,
+                etapa: 'no empezado',
+              },
+              {
+                tarea: 'Cargar y lanzar la aplicación en el servidor',
+                hecho: false,
+                etapa: 'no empezado',
+              },
+              {
+                tarea: 'Configurar el dominio (DNS, Domain, SSL)',
+                hecho: false,
+                etapa: 'no empezado',
+              },
+            ],
+          },
+          {
+            objetivo: 'Hacer las pruebas del proyecto',
+            tareas: [
+              {
+                tarea: 'Ejecutar la matriz de pruebas',
+                hecho: false,
+                etapa: 'no empezado',
+              },
+              {
+                tarea: 'Ajustes a la aplicación',
+                hecho: false,
+                etapa: 'no empezado',
+              },
+              {
+                tarea: 'Seguimiento de la matriz de pruebas',
+                hecho: false,
+                etapa: 'no empezado',
+              },
+            ],
+          },
+          {
+            objetivo: 'Lanzar el producto',
+          },
+        ],
+      },
+    ],
+  };
   constructor(private _befService: NgxBootstrapExpandedFeaturesService) {
     afterRender(() => {
       if (!this._befService) {
